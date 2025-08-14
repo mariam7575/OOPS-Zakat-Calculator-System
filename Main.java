adfd5e2061ed560dd0b23662811bbbad245448db
Maryam Islam <maryam.islam.232@northsouth.edu>
2025-08-15 01:25:59 +0600

    main file added

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String []args) {
+        User user = new User("Mariam");
+        user.addAsset(new Cash(50000.0));
+        user.addAsset(new Gold(80.0, 8000));
+        user.addDebt(10000);
+        ZakatCalculator c = new ZakatCalculator(user);
+        if (c.isEligible())
+            System.out.println("Amount of Zakat" + c.zakatCalculator());
+        else
+            System.out.println("Not eligible for zakat");
+
+
+    }
+
 }
